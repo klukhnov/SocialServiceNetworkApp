a400d1c63168b5cb7ae087c539bac2446aca5a53
Konstantin <klukhnov@gmail.com>
2018-07-21 19:52:20 +0200

    Work on PostActivity and MainActivity.

---
+++ app/src/main/java/com/example/home/socialservicenetworkapp/PostActivity.java
@@ -0,0 +1,68 @@
+package com.example.home.socialservicenetworkapp;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class PostActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ImageButton SelectPostImage;
+    private Button UpdatePostButton;
+    private EditText PostDescription;
+    private static final int Gallery_Pick = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        SelectPostImage = findViewById(R.id.select_post_image);
+        UpdatePostButton = findViewById(R.id.update_post_button);
+        PostDescription = findViewById(R.id.post_description);
+
+
+        mToolbar = findViewById(R.id.update_post_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update post");
+
+        SelectPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenGallery();
+            }
+        });
+    }
+
+    private void OpenGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, Gallery_Pick);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            SendUserToMainActivity();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(PostActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+    }
+}
