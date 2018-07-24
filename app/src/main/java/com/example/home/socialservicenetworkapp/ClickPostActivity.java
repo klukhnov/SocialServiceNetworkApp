af5c30b4cfa045363ddd98f21fc1671f924206b7
Konstantin <klukhnov@gmail.com>
2018-07-24 23:53:41 +0200

    Added EditPost Button with AlertDialog window.

--- app/src/main/java/com/example/home/socialservicenetworkapp/ClickPostActivity.java
+++ app/src/main/java/com/example/home/socialservicenetworkapp/ClickPostActivity.java
@@ -1,10 +1,14 @@
 package com.example.home.socialservicenetworkapp;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -61,6 +65,13 @@ public class ClickPostActivity extends AppCompatActivity {
                         DeletePostButton.setVisibility(View.VISIBLE);
                         EditPostButton.setVisibility(View.VISIBLE);
                     }
+
+                    EditPostButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            EditCurrentPost(description);
+                        }
+                    });
                 }
             }
 
@@ -76,6 +87,33 @@ public class ClickPostActivity extends AppCompatActivity {
                 DeleteCurrentPost();
             }
         });
+    }
+
+    private void EditCurrentPost(String description) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ClickPostActivity.this);
+        builder.setTitle("Edit Post");
+
+        final EditText inputField = new EditText(ClickPostActivity.this);
+        inputField.setText(description);
+        builder.setView(inputField);
+
+        builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ClickPostRef.child("description").setValue(inputField.getText().toString());
+                Toast.makeText(ClickPostActivity.this, "Post updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        Dialog dialog = builder.create();
+        dialog.show();
+
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.holo_orange_dark);
     }
 
     private void DeleteCurrentPost() {
